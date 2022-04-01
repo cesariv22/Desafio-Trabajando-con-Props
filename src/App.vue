0a07361172e81ac1b8040e1bac6b2a2cd9081c8b
Cesar Riveros <c-riveros@hotmail.com>
2022-04-01 15:57:09 -0300

    finished challenge

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <form>
+      <div class="mb-3">
+        <h1>Nueva Tarea</h1>
+        <label class="me-3">Tarea</label>
+        <input
+          type="text"
+          v-model="task"
+          class="form-control w-50 d-inline"
+          placeholder="Ingresa nueva tarea"
+        />
+      </div>
+      <button type="button" @click="showTask" class="btn btn-primary">
+        Añadir
+      </button>
+    </form>
+    <ListTask :addTask="newTask" @delete-task="deleteTask"></ListTask>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import ListTask from "@/components/ListTask.vue";
 
 export default {
   name: "App",
+  data() {
+    return {
+      newTask: [],
+      show: true,
+      task: "",
+    };
+  },
   components: {
-    HelloWorld,
+    ListTask,
+  },
+  methods: {
+    showTask() {
+      if (this.task == false) {
+        alert("Ingresar una tarea");
+      } else {
+        this.newTask.push(this.task);
+        return (this.task = "");
+      }
+    },
+    deleteTask(indice) {
+      this.newTask.splice(indice, 1);
+    },
   },
 };
 </script>
---
+++ src/components/ListTask.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <h2 class="mt-2">Listado de Tareas</h2>
+    <ul v-if="addTask.length > 0" class="list-group list-group-">
+      <li
+        v-for="(objTask, i) of addTask"
+        :key="+i"
+        class="list-group-item list-task mx-5"
+      >
+        {{ objTask }}
+        <button
+          @click="btnDeleteTask(i)"
+          class="btn-danger rounded button-delete"
+        >
+          Eliminar
+        </button>
+      </li>
+    </ul>
+    <ul v-else class="fs-3">
+      Sin tareas aún
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ListTask",
+  props: {
+    addTask: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  methods: {
+    showTask() {
+      if (this.task == false) {
+        alert("Ingresar una tarea");
+      } else {
+        this.newTask.push(this.task);
+        return (this.task = "");
+      }
+    },
+    btnDeleteTask(num) {
+      this.$emit("delete-task", num);
+    },
+  },
+};
+</script>
+
+<style>
+.list-task {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
